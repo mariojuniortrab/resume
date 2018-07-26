08a7ab5c6954851140bf3bbfa7169d9be1b1e340
Mário José Ferreia Júnior <mariojuniortrab@yahoo.com.br>
2018-07-26 15:00:16 -0300

    adding change config button

--- src/App.js
+++ src/App.js
@@ -9,21 +9,44 @@ import TwoThirdColumn from './components/TwoThirdColumn';
 import Experience from './components/Experience';
 import Education from './components/Education';
 import config from './config';
+import configUS from './configUS';
+import FloatButton from './components/FloatButton';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      config : config,
+      language : 'PT'
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e){
+    const language = e.target.innerHTML;
+    const conf = language === 'PT' ? config : configUS ;
+    this.setState({
+      language : e.target.innerHTML,
+      config : conf
+    })
+  }
+
   render() {
+    
     return (
       <div>
         <Container>
           <ThirdColum>
-            <Profile {...config.app.profile}/>
+            <Profile {...this.state.config.app.profile}/>
           </ThirdColum>
           <TwoThirdColumn>
-            <Experience {...config.app.experience}/>
-            <Education {...config.app.education}/>
+            <Experience {...this.state.config.app.experience}/>
+            <Education {...this.state.config.app.education}/>
           </TwoThirdColumn>
         </Container>
-        <Footer {...config.app.footer}/>
+        <Footer {...this.state.config.app.footer}/>
+        <FloatButton  language={this.state.language} handleClick = {this.handleClick}/>
       </div>
     );
   }
--- src/configUS.js
+++ src/configUS.js
@@ -1,4 +1,4 @@
-let primaryColor= 'teal';
+let primaryColor= 'red';
 let secondaryColor = 'pink';
 
 export default {
@@ -11,13 +11,13 @@ export default {
             },
             profileData:{
                 color: primaryColor,
-                profession:'Desenvolvedor Web',
-                residence:'Volta Redonda - RJ - Brasil', 
+                profession:'Web Developer',
+                residence:'Rio de Janeiro - Brazil', 
                 email: 'mariojuniortrab@gmail.com',
-                tel: '(71) 99196-0885 - Tim',
+                tel: '+55 71 99196-0885',
             },
             skills:{
-                text:'Habilidades',
+                text:'Skills',
                 color:primaryColor,
                 skillBar:[
                     {color:primaryColor, title:'PHP', value:'90'},
@@ -30,58 +30,58 @@ export default {
                 ]
             },
             languages:{
-                text:'Idiomas',
+                text:'languages',
                 color:primaryColor,
                 languageBar:[
-                    {color:primaryColor, title:'Português', value:'100'},
-                    {color:primaryColor, title:'Inglês', value:'70'},
-                    {color:primaryColor, title:'Espanhol', value:'30'},
+                    {color:primaryColor, title:'Portuguese', value:'100'},
+                    {color:primaryColor, title:'English', value:'70'},
+                    {color:primaryColor, title:'Spanish', value:'30'},
                 ]
             },
         },
         experience:{
-            title: 'Experiência',
+            title: 'work experience ',
             color:primaryColor,
             jobs:[
-                {color:secondaryColor, ocupation:'Gestor de projetos', 
+                {color:secondaryColor, ocupation:'Project Manager', 
                 company:'Infocraft Consultoria e Informática', begin:'Jan 2017', end:'Jan 2018',
-                description:'Planejamento e gerência de sistemas sob as normas da certificação MPS-Br com equipe de até 10 desenvolvedores.', current:false, currentText:'Atualmente'},
+                description:'Planning and systems management under the MPS-Br certification standards with a team of up to 10 developers.', current:false, currentText:'Atualmente'},
 
-                {color:secondaryColor, ocupation:'Analista de sistemas', 
+                {color:secondaryColor, ocupation:'Systems Analyst', 
                 company:'Infocraft Consultoria e Informática', begin:'Jan 2013', end:'Jan 2017',
-                description:'Planejamento e desenvolvimento de softwares em linguagem PHP. Criação de diagramas UML, seguindo os processos de desenvolvimento de softwares propostos pela certificação MPS-Br.', current:false, currentText:'Atualmente'},
+                description:'Planning and development of software in PHP language. Creation of UML diagrams following the software development processes proposed by the MPS-Br certification.', current:false, currentText:'Atualmente'},
 
-                {color:secondaryColor, ocupation:'Desenvolvedor Web', 
+                {color:secondaryColor, ocupation:'Web developer', 
                 company:'Infocraft Consultoria e Informática', begin:'Mar 2012', end:'Jan 2013',
-                description:'Desenvolvimento de sistemas utilizando linguagem PHP com framework ZEND, JavaScript com framework Sencha EXTJS 4 e banco de dados PostgreSQL.', current:false, currentText:'Atualmente'},
+                description:'Development of systems using PHP language with ZEND framework, JavaScript with Sencha EXTJS 4 framework and PostgreSQL database.', current:false, currentText:'Atualmente'},
 
-                {color:secondaryColor, ocupation:'Programador', 
+                {color:secondaryColor, ocupation:'Programmer', 
                 company:'AAbreu Beneficiamentos', begin:'Jun 2010', end:'Jun 2011',
-                description:'Levantamento de requisitos e criação de diagramas UML para implantação do sistema Protheus da Totvs. Manutenção do Sistema AAbreu em VB 6.0 e banco de dados Oracle e manutenção da intranet e do portal, ambos em ASP.', current:false, currentText:'Atualmente'},
+                description:'Requirements gathering and creation of UML diagrams to implement the Protheus Totvs system. Maintenance of the AAbreu System in VB 6.0 and Oracle database and maintenance of the intranet and the portal, both in ASP.', current:false, currentText:'Atualmente'},
 
-                {color:secondaryColor, ocupation:'Programador Júnior', 
+                {color:secondaryColor, ocupation:'Junior programmer', 
                 company:'AAbreu Beneficiamentos', begin:'Jun 2019', end:'Jun 2010',
                 description:'Manutenção do Sistema AAbreu em VB 6.0 e banco de dados Oracle e manutenção da intranet e do portal, ambos em ASP.', current:false, currentText:'Atualmente'},
             ],
         },
         education:{
-            title: 'Escolaridade',
+            title: 'Education',
             color:primaryColor,
             courses:[
-                {color:secondaryColor, name:'Sistemas de Computação', 
+                {color:secondaryColor, name:'Computer Systems', 
                 institution:'Universidade Federal Fluminense (UFF)', begin:'Jan 2018', end:'Dez 2019',
                 description:'',},
-                {color:secondaryColor, name:'Sistemas de Informação (Não completada)', 
-                institution:'Universidade Federal da Bahia (UFBa) - Salvador - BA', begin:'Jan 2012', end:'Dez 2017',
+                {color:secondaryColor, name:'Information Systems (Not completed)', 
+                institution:'Universidade Federal da Bahia (UFBa) - Salvador - BA - Brazil', begin:'Jan 2012', end:'Dez 2017',
                 description:'',},
-                {color:secondaryColor, name:'Técnologo em Sistemas de Informação (EAD) (Não completada)', 
+                {color:secondaryColor, name:'Information Systems Technician (distance learning) (Not completed)', 
                 institution:'UniCesumar', begin:'Jan 2012', end:'Dez 2014',
                 description:'',},
-                {color:secondaryColor, name:'Sistemas de Informação (Não completada)', 
+                {color:secondaryColor, name:'Information Systems (Not completed)', 
                 institution:'UNIFOA - Volta Redonda - RJ', begin:'Jan 2010', end:'Jul 2011',
                 description:'',},
-                {color:secondaryColor, name:'Técnico em Informática', 
-                institution:'Escola Técnica Pandiá Calógeras - Volta Redonda - RJ', begin:'Jan 2006', end:'Dez 2008',
+                {color:secondaryColor, name:'Computer Technician', 
+                institution:'Escola Técnica Pandiá Calógeras - Volta Redonda - RJ - Brazil', begin:'Jan 2006', end:'Dez 2008',
                 description:'',},
             ],
         },
